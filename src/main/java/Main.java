017ecd1d242beca963a2f389be27a0d15fd64ccf
chenrl <raymond2366@outlook.com>
2021-04-13 10:36:47 +0800

    update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,8 +8,37 @@ import java.util.List;
 public class Main {
 
     static <T> WebResponse<List<T>> test(Class<T> clazz) throws IOException {
-        String str = FileUtils.readFile("/Users/chenrl/Desktop/test2.txt");
-        return JSON.parseObject(str, new TypeReference<WebResponse<List<T>>>(){});
+        return JSON.parseObject("{\n" +
+                "    \"code\":\"0\",\n" +
+                "    \"body\":[\n" +
+                "        {\n" +
+                "            \"permissions\":[\n" +
+                "                {\n" +
+                "                    \"isOn\":true,\n" +
+                "                    \"description\":\"管理员\",\n" +
+                "                    \"id\":\"1158980326240948224\",\n" +
+                "                    \"type\":\"管理员\"\n" +
+                "                }\n" +
+                "            ],\n" +
+                "            \"name\":\"管理员\",\n" +
+                "            \"structureIds\":[\n" +
+                "\n" +
+                "            ],\n" +
+                "            \"id\":\"1158980326442274816\",\n" +
+                "            \"pubAuthStructureIds\":[\n" +
+                "                \"a7132b4dbf4348568481ea9d3910f93e\"\n" +
+                "            ],\n" +
+                "            \"channelIds\":[\n" +
+                "                \"13104094068857937920385969428\"\n" +
+                "            ],\n" +
+                "            \"url\":\"/cmdf/v1/upload/image/5e1ea5219ffd541d4067bbfd.jpeg\",\n" +
+                "            \"menuBarIds\":[\n" +
+                "                \"12155486046352875541425648494\"\n" +
+                "            ]\n" +
+                "        }\n" +
+                "    ],\n" +
+                "    \"message\":\"获取成功\"\n" +
+                "}", new TypeReference<WebResponse<List<T>>>(){});
     }
 
     public static void main(String[] args) throws IOException {
